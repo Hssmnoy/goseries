d147861dda31dddb8d43d05296efe3c6c4478a06
DOG_GOD <110244508+nongakka@users.noreply.github.com>
2026-03-12 10:50:25 +0700

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    name: "ซีรี่ย์ 2026",
+    slug: "2026",
+    url: "https://goseries4k.com/category/%e0%b8%94%e0%b8%b9%e0%b8%8b%e0%b8%b5%e0%b8%a3%e0%b8%b5%e0%b9%88%e0%b8%a2%e0%b9%8c-2026/"
+  },
+  {
+    name: "ซีรี่ย์ 2025",
+    slug: "2025",
+    url: "https://goseries4k.com/category/%e0%b8%94%e0%b8%b9%e0%b8%8b%e0%b8%b5%e0%b8%a3%e0%b8%b5%e0%b9%88%e0%b8%a2%e0%b9%8c-2025/"
+  },
+  {
+    name: "ซีรี่ย์จีน",
+    slug: "china",
+    url: "https://goseries4k.com/category/%e0%b8%94%e0%b8%b9-%e0%b8%8b%e0%b8%b5%e0%b8%a3%e0%b8%b5%e0%b9%88%e0%b8%a2%e0%b9%8c%e0%b8%88%e0%b8%b5%e0%b8%99-%e0%b8%ad%e0%b8%ad%e0%b8%99%e0%b9%84%e0%b8%a5%e0%b8%99%e0%b9%8c/"
+  },
+  {
+    name: "ซีรี่ย์ญี่ปุ่น",
+    slug: "japan",
+    url: "https://goseries4k.com/category/%e0%b8%8b%e0%b8%b5%e0%b8%a3%e0%b8%b5%e0%b9%88%e0%b8%a2%e0%b9%8c%e0%b8%8d%e0%b8%b5%e0%b9%88%e0%b8%9b%e0%b8%b8%e0%b9%88%e0%b8%99/"
+  },
+  {
+    name: "ซีรี่ย์ฝรั่ง",
+    slug: "western",
+    url: "https://goseries4k.com/category/%e0%b8%8b%e0%b8%b5%e0%b8%a3%e0%b8%b5%e0%b9%88%e0%b8%a2%e0%b9%8c%e0%b8%9d%e0%b8%a3%e0%b8%b1%e0%b9%88%e0%b8%87/"
+  },
+  {
+    name: "ซีรี่ย์เกาหลี",
+    slug: "korea",
+    url: "https://goseries4k.com/category/%e0%b8%94%e0%b8%b9%e0%b8%8b%e0%b8%b5%e0%b8%a3%e0%b8%b5%e0%b9%88%e0%b8%a2%e0%b9%8c%e0%b9%80%e0%b8%81%e0%b8%b2%e0%b8%ab%e0%b8%a5%e0%b8%b5-%e0%b8%ad%e0%b8%ad%e0%b8%99%e0%b9%84%e0%b8%a5%e0%b8%99%e0%b9%8c/"
+  },
+  {
+    name: "มินิซีรี่ย์",
+    slug: "mini",
+    url: "https://goseries4k.com/category/%e0%b8%a1%e0%b8%b4%e0%b8%99%e0%b8%b4%e0%b8%8b%e0%b8%b5%e0%b8%a3%e0%b8%b5%e0%b9%88%e0%b8%a2%e0%b9%8c/"
+  },
+  {
+    name: "anime",
+    slug: "anime",
+    url: "https://goseries4k.com/category/%e0%b8%94%e0%b8%b9%e0%b8%ad%e0%b8%99%e0%b8%b4%e0%b9%80%e0%b8%a1%e0%b8%b0/"
+  }
+]
